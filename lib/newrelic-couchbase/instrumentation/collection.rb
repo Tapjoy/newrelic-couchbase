11586bb825c0bf8612921b9217afd67bb12134c6
James Hong <ojameso@hotmail.com>
2025-02-20 14:20:27 -0800

    [NPE] Fix get_all -> get_multi

--- lib/newrelic-couchbase/instrumentation/collection.rb
+++ lib/newrelic-couchbase/instrumentation/collection.rb
@@ -26,9 +26,9 @@ module NewRelic
           end
         end
 
-        def get_all_with_newrelic(*args, &blk)
-          NewRelic::Agent::Datastores.wrap('Couchbase', 'get_all', bucket_name, get_upsert_callback(args.inspect)) do
-            get_all_without_newrelic(*args, &blk)
+        def get_multi_with_newrelic(*args, &blk)
+          NewRelic::Agent::Datastores.wrap('Couchbase', 'get_multi', bucket_name, get_upsert_callback(args.inspect)) do
+            get_multi_without_newrelic(*args, &blk)
           end
         end
       end
@@ -45,8 +45,8 @@ end
   alias_method :get, :get_with_newrelic
   alias_method :get_and_touch_without_newrelic, :get_and_touch
   alias_method :get_and_touch, :get_and_touch_with_newrelic
-  alias_method :get_all_without_newrelic, :get_all
-  alias_method :get_all, :get_all_with_newrelic
+  alias_method :get_multi_without_newrelic, :get_multi
+  alias_method :get_multi, :get_multi_with_newrelic
 
   [
     :binary,
@@ -54,7 +54,6 @@ end
     :get_all_replicas,
     :get_and_lock,
     :get_any_replica,
-    :get_multi,
     :insert,
     :lookup_in,
     :lookup_in_all_replicas,
